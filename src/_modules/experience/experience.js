2ff201c2d17e54c2e8c7450ce68a2e8dc67b6d76
migueljrojas <migueljrojas@gmail.com>
2017-05-26 12:37:08 -0300

    Horizontal click drag scroll

--- src/_modules/experience/experience.js
+++ src/_modules/experience/experience.js
@@ -71,6 +71,41 @@ var Experience = function() {
           	$('._job-events').scrollLeft(left);
         }
    });
+   ////////////
+
+    // function
+    function clickAndDragScroll(elementToScroll) {
+        var clicked = false,
+            curXPos = 0,
+            curYPos = 0;
+
+        elementToScroll.mousemove(function(c) {
+            if (clicked === true) {
+                var NcurXPos = c.pageX - (elementToScroll.offset().left),
+                    NcurYPos = c.pageY - (elementToScroll.offset().top);
+                elementToScroll.scrollLeft(elementToScroll.scrollLeft() + (curXPos - NcurXPos));
+                elementToScroll.scrollTop(elementToScroll.scrollTop() + (curYPos - NcurYPos));
+                curXPos = NcurXPos;
+                curYPos = NcurYPos;
+            }
+        });
+
+        elementToScroll.mousedown(function(c) {
+            clicked = true;
+            curXPos = c.pageX - (elementToScroll.offset().left);
+            curYPos = c.pageY - (elementToScroll.offset().top);
+        });
+
+        elementToScroll.mouseup(function() {
+            clicked = false;
+        });
+    }
+
+    // implementation
+    var elementToScroll = $('._job-events');
+    clickAndDragScroll(elementToScroll);
+
+   ///////////
 };
 
 module.exports = Experience;
